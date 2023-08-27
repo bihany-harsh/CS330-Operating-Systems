33a51b6127164e339a8a99906b423141cfd4e25c
bihany-harsh <harshbihany7@gmail.com>
2023-08-27 13:08:58 +0530

    assignment 1 part 1 done

---
+++ Assignment 1/Part1/sqroot.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc == 2){
+		int n = atoi(argv[argc - 1]);
+		double n_d = (double)n;
+		n = (int)sqrt(n_d + 0.5);
+		printf("%d\n", n);
+		return 0;
+	}
+
+	int pid;
+
+	pid = fork();
+
+	if (pid < 0) {
+		perror("fork");
+		exit(-1);
+	}
+
+	if (!pid) { // child
+		int n = atoi(argv[argc - 1]);
+		double n_d = (double)n;
+		n = (int)sqrt(n_d + 0.5);
+
+		sprintf(argv[argc - 1], "%d", n);
+
+		char new_exc[9];
+		strcpy(new_exc, "./");
+		strcat(new_exc, argv[1]);
+		new_exc[8] = '\0';
+
+		char* new_argv[argc];
+
+		for (int i = 1; i < argc; i++) {
+			if (i == 1) {
+				new_argv[i - 1] = new_exc;
+			}
+			else {
+				new_argv[i - 1] = argv[i];
+			}
+		}
+
+		new_argv[argc - 1] = NULL;
+		
+		if (execvp(new_exc, new_argv)) {
+			perror("exec");
+		}
+		exit(-1);
+
+	} else {
+		wait(NULL);
+	}
+
+	return 0;
+}
+
---
+++ Assignment 1/Part1/square.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc == 2){
+		printf("%d\n", atoi(argv[argc - 1])*atoi(argv[argc - 1]));
+		return 0;
+	}
+
+	int pid;
+
+	pid = fork();
+
+	if (pid < 0) {
+		perror("fork");
+		exit(-1);
+	}
+
+	if (!pid) { // child
+		int n = atoi(argv[argc - 1]);
+		n = n*n;
+
+		sprintf(argv[argc - 1], "%d", n);
+
+		char new_exc[9];
+		strcpy(new_exc, "./");
+		strcat(new_exc, argv[1]);
+		new_exc[8] = '\0';
+
+		char* new_argv[argc];
+
+		for (int i = 1; i < argc; i++) {
+			if (i == 1) {
+				new_argv[i - 1] = new_exc;
+			}
+			else {
+				new_argv[i - 1] = argv[i];
+			}
+		}
+
+		new_argv[argc - 1] = NULL;
+		
+		if (execvp(new_exc, new_argv)) {
+			perror("exec");
+		}
+		exit(-1);
+
+	} else {
+		wait(NULL);
+	}
+
+	return 0;
+}
